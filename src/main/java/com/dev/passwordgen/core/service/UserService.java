4df95623dcaf1ec285154a54f5f2cffe1bb03964
Pierre <chris.pierre1999@gmail.com>
2024-04-20 14:00:09 -0300

    refactor/ add dependency injection

--- src/main/java/com/dev/passwordgen/core/service/UserService.java
+++ src/main/java/com/dev/passwordgen/core/service/UserService.java
@@ -2,6 +2,7 @@ package com.dev.passwordgen.core.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dev.passwordgen.core.model.User;
@@ -10,12 +11,8 @@ import com.dev.passwordgen.core.repository.UserRepository;
 @Service
 public class UserService {
 
-    private final UserRepository repository;
+    @Autowired private UserRepository repository;
     
-    public UserService (UserRepository userRepository){
-        this.repository = userRepository; 
-    }
-   
     public List<User> getUsers(){
         return repository.findAll();
     }
