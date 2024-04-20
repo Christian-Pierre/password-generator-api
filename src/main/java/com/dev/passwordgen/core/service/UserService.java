4975d0e08de42b9cea65a940ab8973607affd9ee
Pierre <chris.pierre1999@gmail.com>
2024-04-20 19:25:14 -0300

    feat/ add get user by id

--- src/main/java/com/dev/passwordgen/core/service/UserService.java
+++ src/main/java/com/dev/passwordgen/core/service/UserService.java
@@ -1,6 +1,7 @@
 package com.dev.passwordgen.core.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ public class UserService {
     
     public List<User> getUsers(){
         return repository.findAll();
+    }
+    public Optional<User> getUser(Long id){
+        return repository.findById(id);
     }
     public User saveUser(User user){
         return repository.save(user);
